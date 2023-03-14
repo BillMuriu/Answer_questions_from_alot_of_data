319815732cf1eb385d4b75e662571bf7bdd2c9e6
unknown <njorogekamau63@gmail.com>
2023-03-14 11:57:12 +0300

    os is noise too

--- recursively_summarize.py
+++ recursively_summarize.py
@@ -1,47 +1,11 @@
 import openai
-import os
-from time import time,sleep
 import json
 import textwrap
-import re
 
 
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
-
-
-def save_file(content, filepath):
-    with open(filepath, 'w', encoding='utf-8') as outfile:
-        outfile.write(content)
-
-
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.6, top_p=1.0, tokens=2000, freq_pen=0.25, pres_pen=0.0, stop=['<<END>>']):
-    max_retry = 5
-    retry = 0
-    while True:
-        try:
-            response = openai.Completion.create(
-                engine=engine,
-                prompt=prompt,
-                temperature=temp,
-                max_tokens=tokens,
-                top_p=top_p,
-                frequency_penalty=freq_pen,
-                presence_penalty=pres_pen,
-                stop=stop)
-            text = response['choices'][0]['text'].strip()
-            text = re.sub('\s+', ' ', text)
-            filename = '%s_gpt3.txt' % time()
-            with open('gpt3_logs/%s' % filename, 'w') as outfile:
-                outfile.write('PROMPT:\n\n' + prompt + '\n\n==========\n\nRESPONSE:\n\n' + text)
-            return text
-        except Exception as oops:
-            retry += 1
-            if retry >= max_retry:
-                return "GPT3 error: %s" % oops
-            print('Error communicating with OpenAI:', oops)
-            sleep(1)
 
 
 def gpt3_embedding(content, engine='text-similarity-ada-001'):
@@ -50,11 +14,6 @@ def gpt3_embedding(content, engine='text-similarity-ada-001'):
     save_gpt3_log(content, str(vector))
     return vector
 
-
-def similarity(v1, v2):  # return dot product of two vectors
-    return np.dot(v1, v2)
-
-    
 
 if __name__ == '__main__':
     alltext = open_file('input.txt')
